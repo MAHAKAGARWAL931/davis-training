c168cce66fd900459aab0c30bd2a9d22ca9ad253
MAHAKAGARWAL931 <mahakagarwal650@gmail.com>
2026-04-08 10:25:24 +0530

    Add files via upload

---
+++ classwork/module/calculation.py
@@ -0,0 +1,5 @@
+import numericcalculation
+m=10
+n=20
+print("Sum of ",m,"and",n,"is : ",numericcalculation.addnumbers(m,n))
+print("Sum of ",m,"and",n,"is : ",numericcalculation.productnumbers(m,n))
