50fc2443b93db4cf955866101e837bd4fce21f19
MAHAKAGARWAL931 <mahakagarwal650@gmail.com>
2026-04-09 09:59:24 +0530

    Add files via upload

---
+++ Python_Coding_Challenge-2/multiplication.py
@@ -0,0 +1,8 @@
+# Take input from user
+num = int(input("Enter a number: "))
+
+# Loop from 1 to 10 to generate table
+for i in range(1, 11):
+    
+    # Print multiplication result
+    print(num, "x", i, "=", num * i)
