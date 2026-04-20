46805f44853a7361c87336f0360f6151241aa598
MAHAKAGARWAL931 <mahakagarwal650@gamil.com>
2026-04-20 10:18:34 +0530

    Array creation In Numpy

---
+++ classwork/module/NUMPY IN PYTHON/create_an_array.py
@@ -0,0 +1,9 @@
+import numpy as np
+# tocreate an array
+# numbers = np.array([20,30,40,70,90])
+# print(numbers)
+# numbers = np.array([20,30,4.5,70,90])
+# print(numbers)
+# can be used for data other than numeric
+data = np.array([45,'python',70])
+print(data)
